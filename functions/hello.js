c8d0a4491ffa46e672033365e00d0faa030519fe
xinya585 <84681172+xinya585@users.noreply.github.com>
2026-09-03 22:24:50 +0800

    Remove test file

--- functions/hello.js
+++
@@ -1,6 +0,0 @@
-export async function onRequest(context) {
-  return new Response(JSON.stringify({ message: 'hello from functions', method: context.request.method }), {
-    status: 200,
-    headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' }
-  });
-}
